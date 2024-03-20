076a56b460bf884382d7263fb3eb7779194134f3
Bruno Sakaguchi <108704757+JapaBKS@users.noreply.github.com>
2024-03-20 18:09:54 -0300

    Atualizar o ex08.py

---
+++ ex08.py
@@ -0,0 +1,6 @@
+salario_hora = float(input('Digite o seu salário por hora trabalhada: '))
+horas_trabalhadas = float(input('Digite o número de horas trabalhadas no mês: '))
+
+salario = salario_hora*horas_trabalhadas
+
+print('O seu salário nesse mês é igual a: {}'.format(salario))
