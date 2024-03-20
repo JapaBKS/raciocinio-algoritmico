661e65f1e80de053ef587b558aa5cb343b0d9528
Bruno Sakaguchi <108704757+JapaBKS@users.noreply.github.com>
2024-03-20 17:56:14 -0300

    Atualizar o ex04.py

---
+++ ex04.py
@@ -0,0 +1,9 @@
+n1 = float(input('Digite um número: ')
+n2 = float(input('Digite outro número: ')
+n3 = float(input('Digite outro número: ')
+n4 = float(input('Digite outro número: ')
+
+soma = n1+n2+n3+n4
+media = soma/4
+
+print('A média entre as notas acima é igual a: {:.2f}'.format(media))
