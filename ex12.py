d6b5d4f1c9a5e7d4aa91a579714939cf8ca475e7
Bruno Sakaguchi <bruno.kioshi@pucpr.edu.br>
2024-03-21 17:23:45 -0300

    criação do ex 12

---
+++ ex12.py
@@ -0,0 +1,5 @@
+farenheit = float(input('Digite uma temperatura em graus Farenheit: '))
+
+celsius = (5*(farenheit-32)/9)
+
+print('\nA temperatura de {:.2f}ºF em graus Celsius é igua a: {:.2f}ºC\n'.format(farenheit, celsius))
