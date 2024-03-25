0cc7dc9ff9e94d2631fc86ede333742185ca8613
Bruno Sakaguchi <bruno.kioshi@pucpr.edu.br>
2024-03-25 18:37:22 -0300

    criação do PBL01

---
+++ PBL01.py
@@ -0,0 +1,19 @@
+salario = float(input('Digite o seu salário: R$'))
+
+if salario <= 2259.20:
+    aliquota = 0
+    parcela = 0
+elif salario > 2259.20 and salario <= 2826.65:
+    aliquota = 7.5
+    parcela = 169.44
+elif salario > 2826.65 and salario <= 3751.05:
+    aliquota = 15
+    parcela = 381.44
+elif salario > 3751.05 and salario <= 4664.68:
+    aliquota = 22.5
+    parcela = 662.77
+else:
+    aliquota = 27.5
+    parcela = 896
+
+print('\nVocê pagará uma aliquota de {}%. Valor descontado: R${:.2f}.\n'.format(aliquota, parcela))
