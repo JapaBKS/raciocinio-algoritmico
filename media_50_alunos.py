4f0a656ded20e0c65c8db54cb86e76d9b0b437ca
Bruno Sakaguchi <108704757+JapaBKS@users.noreply.github.com>
2024-04-02 21:04:10 -0300

    troca de while por for

--- media_50_alunos.py
+++ media_50_alunos.py
@@ -1,15 +1,14 @@
 media_total = 0
-i = 1
 
-while(i<=5):
-    print('Aluno', i)
+for i in range(5):
+    print('Aluno', i+1)
     n1 = float(input('Digite a nota 1: '))
     n2 = float(input('Digite a nota 2: '))
     n3 = float(input('Digite a nota 3: '))
     n4 = float(input('Digite a nota 4: '))
 
     media_aluno = (n1+n2+n3+n4)/4
-    media_total = media_total + media_aluno
+    media_total += media_aluno
 
     if media_aluno >= 7:
         print('APROVADO! Média', media_aluno)
@@ -18,6 +17,6 @@ while(i<=5):
     i+=1
     print('')
 
-media_total = media_total/(i-1)
+media_total = media_total/(i)
 
-print('A média dos', i-1, 'alunos é igual a', media_total)
+print('A média dos', i, 'alunos é igual a', media_total)
