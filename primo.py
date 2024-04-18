7670e5ba007fded855d4a997f6983b64e6356cd5
Bruno Sakaguchi <bruno.kioshi@pucpr.edu.br>
2024-04-18 15:10:27 -0300

    correção

--- primo.py
+++ primo.py
@@ -1,11 +1,12 @@
 n = int(input('Digite um número para verificar se ele é primo: '))
+
 eh_primo = 1
 
 for i in range(2, n):
     if n%i == 0:
         eh_primo = 0
         break
-    
+
 if eh_primo:
     print(f'O número {n} é primo!')
 else:
