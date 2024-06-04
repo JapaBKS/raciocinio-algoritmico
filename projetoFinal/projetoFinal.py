594173320d73e0df1b0b7c310f41bbeffc865b37
Bruno Sakaguchi <bruno.kioshi@pucpr.edu.br>
2024-06-04 20:03:47 -0300

    ajustes de pastas

---
+++ projetoFinal/projetoFinal.py
@@ -0,0 +1,47 @@
+bebidas = {
+    'Refrigerantes': {
+        'Nome': 'Coca-Cola', 'Preço': 6,
+        'Nome': 'Pepsi', 'Preço': 6,
+        'Nome': 'Guaraná Jesus', 'Preço': 6,
+        'Nome': 'Fanta Uva', 'Preço': 6
+    },
+    'Alcoólicos': {
+        'Nome': 'Vinho', 'Preço': 60,
+        'Nome': 'Whiskey', 'Preço': 35,
+        'Nome': 'Gin Tônica', 'Preço': 35,
+        'Nome': 'Drinks', 'Preço': 30
+    },
+    'Sucos': {
+        'Nome': 'Abacaxi', 'Preço': 15,
+        'Nome': 'Maracujá', 'Preço': 15,
+        'Nome': 'Limão', 'Preço': 15,
+        'Nome': 'Laranja', 'Preço': 15,
+        'Nome': 'Manga', 'Preço': 15
+    },
+    'Quentes': {
+        'Nome': 'Expresso', 'Preço': 5,
+        'Nome': 'Capuccino', 'Preço': 10,
+        'Nome': 'Mocaccino', 'Preço': 12,
+        'Nome': 'Café com Leite', 'Preço': 7,
+        'Nome': 'Achocolatado', 'Preço': 10,
+        'Nome': 'Chá', 'Preço': 7
+    }
+}
+
+entradas = {
+    'Salada': {
+        'Nome': 'Salada', 'Preço': 30
+    },
+    'Sopa': {
+        'Nome': 'Sopa', 'Preço': 30
+    },
+    'Porção de Frios': {
+        'Nome': 'Porção de Frios', 'Preço': 30
+    },
+    'Batatas Fritas': {
+        'Nome': 'Batatas Fritas', 'Preço': 30
+    },
+    'Polenta': {
+        'Nome': 'Polenta', 'Preço': 25
+    }
+}
